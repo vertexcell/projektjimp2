0dc47b6490a12fa643491a7606e0808a39a1b56d
vertexcell <vertexcell@gmail.com>
2024-04-14 23:21:43 +0200

    Implementacja kolejki

--- queue.c
+++ queue.c
@@ -1,1 +1,42 @@
+#include "queue.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void initiate_queue(queue_t *q){
+    q->front = NULL;
+    q->end = NULL;
+}
+
+void push(queue_t *q, int value){
+    node_t *new = (node_t*)malloc(sizeof(node_t));
+    new->data = value;
+    new->next = NULL;
+    if(q->front == NULL){
+        q->front = new;
+    } else {
+        q->end->next = new;
+    }
+    q->end = new;
+}
+
+int pop(queue_t *q){
+    if(q->front == NULL){
+        return;
+    }
+    node_t *temp = q->front;
+    int removed = temp->data;
+    q->front = q->front->next;
+    if(q->front == NULL){
+        q->end = NULL;
+    }
+    free(temp);
+    return removed;
+}
+
+void drop_queue(queue_t *q){
+    while(q->front != NULL){
+        pop(q);
+    }
+    free(q);
+}
---
+++ queue.h
@@ -0,0 +1,17 @@
+typedef struct node_t {
+    int data;
+    struct node_t *next;
+} node_t;
+
+typedef struct queue_t {
+    node_t* front;
+    node_t* end;
+} queue_t;
+
+void initiate_queue(queue_t *q);
+
+void push(queue_t *q, int value);
+
+int pop(queue_t *q);
+
+void drop_queue(queue_t *q);
